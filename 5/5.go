d0a4f31ebec410f31700210919587c093587dc28
Komronbek Muhammadjonov <komronbekmuhammadjonov11@gmail.com>
2023-03-13 13:34:06 +0500

    5 task done

--- 5/5.go
+++ 5/5.go
@@ -5,15 +5,25 @@ import (
 )
 
 func main() {
-	fmt.Println(longestPalindrome("qwfgwdabaqw"))
+	fmt.Println(longestPalindrome("aziza"))
 }
 
 func longestPalindrome(s string) string {
+	if len(s) == 1 {
+		return s
+	}
 	var longestStr string
 	for i := 0; i < len(s); i++ {
-		for j := 0; j <= i; j++ {
-
+		for j := 0; j < len(s); j++ {
+			if i < j && isPalindrome(s[i:j+1]) {
+				if len(s[i:j+1]) > len(longestStr) {
+					longestStr = s[i : j+1]
+				}
+			}
 		}
+	}
+	if len(longestStr) == 0 {
+		return s[0:1]
 	}
 	return longestStr
 }
